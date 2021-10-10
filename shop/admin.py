3dd29565cdf8cd4e21b5b9c67e9ed921a2d63fb0
Maksim Litvinenka <letuwwa@gmail.com>
2021-10-10 21:23:39 +0300

    feature: db models and requirements

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Detail
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_title']
+
+
+@admin.register(Detail)
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ['detail_title', 'price', 'available', 'created_at', 'updated_at']
+    list_filter = ['available', 'created_at', 'updated_at']
+    list_editable = ['price', 'available']
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_title = models.CharField(max_length=128, db_index=True, verbose_name="Название категории")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_title
+
+    class Meta:
+        ordering = ('category_title',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Detail(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
+    available = models.BooleanField(default=True, verbose_name="В наличии")
+    country = models.CharField(max_length=128, verbose_name="Страна производства")
+    description = models.TextField(max_length=2048, verbose_name="Описание товара")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость товара")
+    detail_title = models.CharField(max_length=1024, db_index=True, verbose_name="Название товара")
+    image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True, verbose_name="Фотография товара")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.detail_title
+
+    class Meta:
+        ordering = ('detail_title',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
