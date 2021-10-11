7ea2a1e0da1fb3b2ce87dfaafea98cd417d2a2d6
Maksim Litvinenka <letuwwa@gmail.com>
2021-10-12 00:18:21 +0300

    feature: slug url and readme

--- auto_details_shop/settings.py
+++ auto_details_shop/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.core.servers.basehttp import WSGIServer
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+WSGIServer.handle_error = lambda *args, **kwargs: None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
